9c10b49ce1a6636113b5bb3af0dddd70d4c016b7
Itai Cohen <34794316+itai590@users.noreply.github.com>
2018-01-17 01:30:11 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace ListViewDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnClear = new System.Windows.Forms.Button();
+            this.cmb_PortName = new System.Windows.Forms.ComboBox();
+            this.chkConnect = new System.Windows.Forms.CheckBox();
+            this.comPort = new System.IO.Ports.SerialPort(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label7 = new System.Windows.Forms.Label();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.AllowColumnReorder = true;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.listView1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(0, 1);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(934, 250);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Measure No.";
+            this.columnHeader1.Width = 95;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Date & Time";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader2.Width = 170;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Temperature  1 [מעלות צלזיוס]";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader3.Width = 220;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Gaz 1 [ppm]";
+            this.columnHeader4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader4.Width = 100;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Temperature  2 [מעלות צלזיוס]";
+            this.columnHeader5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader5.Width = 220;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Gaz 2 [ppm]";
+            this.columnHeader6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader6.Width = 100;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(848, 257);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // cmb_PortName
+            // 
+            this.cmb_PortName.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmb_PortName.FormattingEnabled = true;
+            this.cmb_PortName.Location = new System.Drawing.Point(181, 282);
+            this.cmb_PortName.Name = "cmb_PortName";
+            this.cmb_PortName.Size = new System.Drawing.Size(97, 21);
+            this.cmb_PortName.TabIndex = 7;
+            // 
+            // chkConnect
+            // 
+            this.chkConnect.Appearance = System.Windows.Forms.Appearance.Button;
+            this.chkConnect.CheckAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.chkConnect.Location = new System.Drawing.Point(181, 309);
+            this.chkConnect.Name = "chkConnect";
+            this.chkConnect.Size = new System.Drawing.Size(97, 23);
+            this.chkConnect.TabIndex = 6;
+            this.chkConnect.Text = "Connect";
+            this.chkConnect.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.chkConnect.UseVisualStyleBackColor = true;
+            this.chkConnect.CheckedChanged += new System.EventHandler(this.chkConnect_CheckedChanged);
+            this.chkConnect.Click += new System.EventHandler(this.chkConnect_Click);
+            // 
+            // comPort
+            // 
+            this.comPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.comPort_DataReceived);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(330, 309);
+            this.progressBar1.Maximum = 3000;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(112, 23);
+            this.progressBar1.Step = 25;
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 10;
+            this.progressBar1.Value = 1000;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(260, 244);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 31);
+            this.label1.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label2.Location = new System.Drawing.Point(21, 280);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 19);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "DD/MM/YYYY";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label3.Location = new System.Drawing.Point(21, 302);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 19);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "HH:MM:SS";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.Control;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label4.Location = new System.Drawing.Point(327, 287);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(117, 16);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Recieving Data...";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label5.Location = new System.Drawing.Point(178, 260);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 16);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Choose Port:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Tahoma", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label6.Location = new System.Drawing.Point(22, 257);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 16);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Today:";
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 500;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Teal;
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label7.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(475, 314);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(111, 18);
+            this.label7.TabIndex = 102;
+            this.label7.Text = "warning_where";
+            // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            this.timer3.Interval = 1;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // label8
+            // 
+            this.label8.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Red;
+            this.label8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label8.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(600, 257);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(177, 41);
+            this.label8.TabIndex = 103;
+            this.label8.Text = "Warning!!";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label9.ForeColor = System.Drawing.Color.Blue;
+            this.label9.Location = new System.Drawing.Point(327, 342);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(303, 14);
+            this.label9.TabIndex = 104;
+            this.label9.Text = "Wait until the gas sensors warm up, time to GO:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label10.ForeColor = System.Drawing.Color.Blue;
+            this.label10.Location = new System.Drawing.Point(629, 342);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(98, 14);
+            this.label10.TabIndex = 105;
+            this.label10.Text = "mm:ss minutes";
+            // 
+            // timer4
+            // 
+            this.timer4.Enabled = true;
+            this.timer4.Interval = 120000;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(935, 378);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.cmb_PortName);
+            this.Controls.Add(this.chkConnect);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.listView1);
+            this.Font = new System.Drawing.Font("Tahoma", 8.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.ComboBox cmb_PortName;
+        private System.Windows.Forms.CheckBox chkConnect;
+        private System.IO.Ports.SerialPort comPort;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Timer timer4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Media;
+using System.Timers;
+
+
+namespace ListViewDemo
+{
+
+    public partial class Form1 : Form
+    {
+        string c0, c1, c2, c3, c4, c5, Lo;
+        int state, num = 1, num_temp, gas1, gas2, la_78_en = 0, pb_cnt = 0, pb_rst = 0, fr = 1, seconds = 0, minutes = 5, sec_en = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            cmb_PortName.Items.AddRange(System.IO.Ports.SerialPort.GetPortNames()); // insert all ports
+            state = 0;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            num = 1;
+            la_78_en = 0;
+            label7.Hide();
+            label8.Hide();
+        }
+
+        private void chkConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (chkConnect.Checked == true)
+                {
+                    progressBar1.Show();
+                    label4.Show();
+                    comPort.PortName = cmb_PortName.Text;
+                    comPort.Open();
+                    chkConnect.Text = "Disconnect";
+                    cmb_PortName.Enabled = false;
+                }
+                else
+                {
+                    progressBar1.Hide();
+                    progressBar1.Value = progressBar1.Minimum;
+                    label4.Hide();
+                    label4.Text = "Recieving Data";
+                    comPort.Close();
+                    chkConnect.Text = "Connect";
+                    cmb_PortName.Enabled = true;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                progressBar1.Hide();
+                label4.Hide();
+                MessageBox.Show(ex.Message);
+            }
+        }
+        /*port selection*/
+        private void comPort_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            byte data;
+            ListViewItem item;
+
+            while (comPort.BytesToRead != 0)
+            {
+                data = Convert.ToByte(comPort.ReadByte());
+                switch (state)
+                {
+                    case 0:
+                        if (data == 0xAA)
+                            state = 1;
+                        pb_cnt = 1;
+                        break;
+                    case 1: c2 = data.ToString();
+                        state = 2;
+                        break;
+                    case 2: c3 = (data * 100).ToString();
+                        state = 3;
+                        gas1 = data * 100;
+                        break;
+                    case 3: c4 = data.ToString();
+                        state = 4;
+                        break;
+                    case 4: c5 = (data * 100).ToString();
+                        state = 5;
+                        gas2 = data * 100;
+                        break;
+                    case 5: c1 = DateTime.Now.Day + "/" + DateTime.Now.Month + "/" + DateTime.Now.Year + " " + DateTime.Now.Hour + ":" + DateTime.Now.Minute + ":" + DateTime.Now.Second;
+                        state = 6;
+                        break;
+                    case 6: c0 = num.ToString();
+                        num++;
+
+                        item = new ListViewItem(c0);
+                        item.SubItems.Add(c1);
+                        item.SubItems.Add(c2);
+                        item.SubItems.Add(c3);
+                        item.SubItems.Add(c4);
+                        item.SubItems.Add(c5);
+
+                        if (gas1 > 700 || gas2 > 700)
+                        {
+                            la_78_en = 1;
+                            SystemSounds.Beep.Play();
+                            item.BackColor = Color.Red;
+                            item.ForeColor = Color.White;
+                            num_temp = num - 1;
+                            if (gas1 > 700 && gas2 > 700)
+                            {
+                                Lo = "1,2";
+                            }
+                            else if (gas1 > 700)
+                            {
+                                Lo = "1";
+                            }
+                            else Lo = "2";
+                        }
+
+                        pb_cnt = 1;
+
+                        listView1.Invoke(new EventHandler(delegate
+                        {
+                            listView1.Items.Add(item);
+                        }));
+
+                        pb_rst = 1;
+                        state = 0;
+                        break;
+                    default: break;
+                }
+
+            }
+        }
+
+        private void chkConnect_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //update runing clock
+            label3.Text = DateTime.Now.Hour + ":" + DateTime.Now.Minute + ":" + DateTime.Now.Second;
+            label2.Text = DateTime.Now.Day + "/" + DateTime.Now.Month + "/" + DateTime.Now.Year;
+
+            //recieving Data runing
+            if (label4.Visible == true)
+            {
+                label4.Text = label4.Text + ".";
+            }
+            if (label4.Text.Equals("Recieving Data...."))
+            {
+                label4.Text = "Recieving Data";
+            }
+
+            //counting down clock
+            if (minutes == 0 && seconds == 0)
+            {
+                label10.Hide();
+                label9.ForeColor = Color.Green;
+                label9.Text = "Gas sensors are ready to use";
+                sec_en = 0;
+            }
+            if (seconds == 0)
+            {
+                seconds = 60;
+                minutes = minutes - 1;
+            }
+            if (sec_en == 1)
+            {
+                seconds = seconds - 1;
+                label10.Text = minutes + ":" + (seconds).ToString() + " minutes";
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            //progress bar runing config
+            if (pb_rst == 1)
+            {
+                progressBar1.Value = progressBar1.Minimum;
+            }
+            if (pb_cnt == 1)
+            {
+                progressBar1.Increment(progressBar1.Maximum / 2);
+                if (fr == 1)
+                {
+                    progressBar1.Increment(progressBar1.Maximum / 2);
+                    fr = 0;
+                }
+                pb_cnt = 0;
+            }
+            pb_rst = 0;
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            if (la_78_en == 1)
+            {
+                label7.Text = "Dangerous concentration was measured at measure No." + num_temp + " (Gas " + Lo + ")";
+                label7.Show();
+                label8.Show();
+            }
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            if (sec_en == 0)
+            {
+                label9.Hide();
+            }
+        }
+
+        private void cmb_PortName_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+        /*-----init-- */
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;//start timer #1
+            timer2.Enabled = true;//start timer #2
+            progressBar1.Hide();
+            progressBar1.Value = progressBar1.Minimum; 
+           
+            label4.Hide();
+            label7.Hide();
+            label8.Hide();
+        }
+    }
+}
